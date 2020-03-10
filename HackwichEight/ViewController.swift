ad7cfd0a66c22d52cfd5e4b26bda3d2c46816942
Joseph Payongayong <spbeachhi@gmail.com>
2020-03-10 10:14:02 -1000

    Parts 1-7 completed

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        print("The value of the slider is:\(slider.value)")
+    }
+    @IBAction func guessNumberPressed(_ sender: Any) {
+    //1. create alert view
+        let alert = UIAlertController(title:"Hello World!", message: "This is my first alert view", preferredStyle: .alert)
+        
+    //2. button that user taps to dismiss view controller
+        let action = UIAlertAction(title: "Awesome" ,style: .default, handler: nil)
+        
+    //3. add the button to the alert view
+        alert.addAction(action)
+        
+    //4. present alertview on the screen
+        present(alert, animated: true, completion : nil)
+    }
+    
 }
 
